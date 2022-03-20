d387e755722637c0caa7f21af6e20740028bc6fa
Eugeniu Ceban <83615676+DMIT-CSD-Eugeniu-Ceban@users.noreply.github.com>
2022-03-19 23:08:11 -0600

    ,

--- src/components/sidebar/SideBar.js
+++ src/components/sidebar/SideBar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 import {
     IoGridOutline,
@@ -14,14 +14,16 @@ import {
     IoCubeSharp,
     IoCopyOutline,
     IoChevronDownSharp,
-    IoChevronUpSharp,
+    IoAddSharp,
+    IoHandLeftSharp,
+    IoGlassesOutline,
 } from "react-icons/io5";
 
-import { SideBarSection } from './../../ui/sideBarButton'
-import { SidebarContainer } from './../../ui/sideContainer'
-import { Sections, H3 } from './styles'
+import { SideBarSection } from "./../../ui/sideBarButton";
+import { SidebarContainer } from "./../../ui/sideContainer";
+import { Sections, H3 } from "./styles";
 
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 function SideBar(props) {
     return (
@@ -29,27 +31,20 @@ function SideBar(props) {
             <SidebarContainer>
                 <SideBarSection>
                     <div style={{ display: "flex" }}>
-                        <IoGridOutline
-                            size="1.4rem"
-                            style={{ color: "#9C27B0" }}
-                        />
-                        {/* <H3>DASHBOARD</H3> */}
+                        <IoGridOutline size="1.4rem" />
                         <Link
                             to="/dashboard"
                             style={{
-                                textTransform: "uppercase",
                                 color: "black",
                                 paddingLeft: "0.5rem",
-                                textDecoration: "underline",
-                                fontWeight: "500",
                             }}
                         >
-                            Dashboard
+                            DASHBOARD
                         </Link>
                     </div>
-                    {/* <div>
+                    <div>
                         <IoChevronForward />
-                    </div> */}
+                    </div>
                 </SideBarSection>
             </SidebarContainer>
 
@@ -86,15 +81,58 @@ function SideBar(props) {
                     </div>
                 </SideBarSection>
 
-                <SideBarSection style={{ marginBottom: "1.5rem" }}>
-                    <div style={{ display: "flex" }}>
+                <SideBarSection style={{ marginBottom: "0.5rem" }}>
+                    <div style={{ display: "flex", color: "blueviolet" }}>
                         <IoLibrarySharp size="1.4rem" />
-                        <H3>PRODUCTS</H3>
+                        <H3
+                            style={{
+                                textDecoration: "underline",
+                                fontWeight: "650",
+                            }}
+                        >
+                            PRODUCTS
+                        </H3>
                     </div>
                     <div>
                         <IoChevronDownSharp />
                     </div>
                 </SideBarSection>
+
+                <div className="multiButton">
+                    <button
+                        className="button"
+                        style={{
+                            paddingLeft: "unset",
+                            color: "green",
+                            fontWeight: "600",
+                        }}
+                    >
+                        <IoAddSharp />
+                        &nbsp;Add New Product
+                    </button>
+                    <button
+                        className="button"
+                        style={{
+                            paddingLeft: "unset",
+                            color: "darkcyan",
+                            fontWeight: "600",
+                        }}
+                    >
+                        <IoHandLeftSharp />
+                        &nbsp; Edit Product
+                    </button>
+                    <button
+                        className="button"
+                        style={{
+                            paddingLeft: "unset",
+                            color: "orangered",
+                            fontWeight: "600",
+                        }}
+                    >
+                        <IoGlassesOutline />
+                        &nbsp;View All Products
+                    </button>
+                </div>
 
                 <SideBarSection style={{ marginBottom: "1.5rem" }}>
                     <div style={{ display: "flex" }}>
--- src/components/sidebar/styles.js
+++ src/components/sidebar/styles.js
@@ -1,12 +1,71 @@
+import { Button } from 'bootstrap';
 import styled from 'styled-components';
 
 const Sections = styled.div`
-  background-color: #ECEFF1;
-  width: 12.10rem;
-  border-top: 1px solid #78909C;
-  border-right: 1px solid #78909C;
-  height: 92.5vh;
-`
+    background-color: #eceff1;
+    width: 12.1rem;
+    border-top: 1px solid #78909c;
+    border-right: 1px solid #78909c;
+    height: 92.5vh;
+
+    button {
+        cursor: pointer;
+        border: unset;
+    }
+
+    .multiButton {
+        margin: 0 0 0.8rem 0;
+        padding-left: 0.3rem;
+    }
+
+    .multiButton > button {
+        margin: 0.4rem 0;
+    }
+
+    .button {
+        position: relative;
+        border-radius: 3px;
+        color: white;
+        user-select: none;
+        transition-duration: 0.4s;
+        -webkit-transition-duration: 0.4s;
+    }
+
+    .button:hover {
+        transition-duration: 0.5s;
+        background-color: silver;
+    }
+
+    .button:after {
+        content: "";
+        display: block;
+        position: absolute;
+        border-radius: 3px;
+        left: 0;
+        top: 0;
+        width: 100%;
+        height: 100%;
+        opacity: 0;
+        transition: all 0.5s;
+        box-shadow: 0 0 10px 40px white;
+    }
+
+    .button:active:after {
+        box-shadow: 0 0 0 0 white;
+        position: absolute;
+        border-radius: 3px;
+        left: 0;
+        top: 0;
+        opacity: 1;
+        transition: 0s;
+    }
+
+    .button:active {
+        top: 1px;
+        border: none;
+        outline: none;
+    }
+`;
 
 const H3 = styled.h3`
   margin-left: 0.5rem;
